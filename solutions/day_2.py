824cafb3c45426af4a4e1387529227971f91b66f
Albuser <alexanderbuser@gmail.com>
2023-12-02 10:03:32 -0500

    day two

---
+++ solutions/day_2.py
@@ -0,0 +1,27 @@
+import re
+
+myInput = open("inputs/day_2.txt").readlines()
+
+# Part One
+
+def parseLine(line):
+    gameId = re.search(r'Game (\d+):', line).group(1)
+    reds =   [int(x) <= 12 for x in re.findall(r'(\d+) red',   line)]
+    greens = [int(x) <= 13 for x in re.findall(r'(\d+) green', line)]
+    blues =  [int(x) <= 14 for x in re.findall(r'(\d+) blue',  line)]
+    if all(reds) and all(greens) and all(blues):
+        return int(gameId)
+    return 0
+
+print(sum(list(map(lambda line: parseLine(line), myInput))))
+
+# Part Two
+
+def parseLine(line):
+    gameId = re.search(r'Game (\d+):', line).group(1)
+    reds =   [int(x) for x in re.findall(r'(\d+) red',   line)]
+    greens = [int(x) for x in re.findall(r'(\d+) green', line)]
+    blues =  [int(x) for x in re.findall(r'(\d+) blue',  line)]
+    return max(reds)*max(greens)*max(blues)
+
+print(sum(list(map(lambda line: parseLine(line), myInput))))
