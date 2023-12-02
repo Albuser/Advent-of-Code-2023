20d15d5189c79990bdc98bacb336578532c83ab2
Albuser <alexanderbuser@gmail.com>
2023-12-02 11:31:53 -0500

    alignment

--- solutions/day_2.py
+++ solutions/day_2.py
@@ -7,9 +7,9 @@ myInput = open("inputs/day_2.txt").readlines()
 
 def parseLine(line):
     gameId = re.search(r'Game (\d+):', line).group(1)
-    reds =   [int(x) <= 12 for x in re.findall(r'(\d+) red',   line)]
+    reds   = [int(x) <= 12 for x in re.findall(r'(\d+) red',   line)]
     greens = [int(x) <= 13 for x in re.findall(r'(\d+) green', line)]
-    blues =  [int(x) <= 14 for x in re.findall(r'(\d+) blue',  line)]
+    blues  = [int(x) <= 14 for x in re.findall(r'(\d+) blue',  line)]
     if all(reds) and all(greens) and all(blues):
         return int(gameId)
     return 0
