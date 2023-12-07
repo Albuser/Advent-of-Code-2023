2b8872cbe6ca7095a31703ee7068272e1c6248f3
Albuser <alexanderbuser@gmail.com>
2023-12-07 12:57:36 -0500

    cleanup

--- solutions/day_7.py
+++ solutions/day_7.py
@@ -1,4 +1,3 @@
-import re
 import functools
 import copy
 myInput = [line.strip() for line in open("inputs/day_7.txt").readlines()]
@@ -6,20 +5,25 @@ myInput = [line.strip() for line in open("inputs/day_7.txt").readlines()]
 pt1LetterMapping = {'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
 pt2LetterMapping = {'T': 10, 'Q': 11, 'K': 12, 'A': 13, 'J': 0}
 
+
 def parseCard(char, mapping):
     if char.isdigit():
+        assert (int(char) > 1)
         return int(char)
+    assert (char in mapping)
     return mapping.get(char)
+
 
 class Hand:
     def __init__(self, inputLine, isPartTwo=False):
+        curMap = pt1LetterMapping
         if isPartTwo:
             curMap = pt2LetterMapping
-        else:
-            curMap = pt1LetterMapping
         self.cards = [parseCard(char, curMap) for char in inputLine.split(' ')[0]]
         self.bid = int(inputLine.split(' ')[1])
         self.type = self.getHandType(isPartTwo)
+        assert (len(self.cards) == 5)
+        assert (self.type in range(7))
     
     def getHandType(self, isPartTwo):
         if isPartTwo:
@@ -35,20 +39,20 @@ class Hand:
         else:
             tempCards = self.cards
 
-        multiplicities = [len([card for card in tempCards if card == curCard]) for curCard in set(tempCards)]
-        pairs = [val for val in multiplicities if val == 2]
-        assert(sum(multiplicities) == 5)
-        if len(multiplicities) == 5:  # High Card
+        cardFreqs = [len([card for card in tempCards if card == curCard]) for curCard in set(tempCards)]
+        pairs = [val for val in cardFreqs if val == 2]
+
+        if len(cardFreqs) == 5:  # High Card
             return 0
-        if len(multiplicities) == 4:  # One Pair
+        if len(cardFreqs) == 4:  # One Pair
             return 1
         if len(pairs) == 2:  # Two Pair
             return 2
-        if (len(multiplicities) == 3):
+        if (len(cardFreqs) == 3):
             return 3  # Three of a Kind
         if len(pairs) == 1:
             return 4  # Full House
-        if (len(multiplicities) == 2):
+        if (len(cardFreqs) == 2):
             return 5  # Four of a Kind
         return 6  # Five of a Kind
     
@@ -58,20 +62,18 @@ class Hand:
         for i in range(5):
             if self.cards[i] != otherHand.cards[i]:
                 return self.cards[i] - otherHand.cards[i]
-            
-def compareHands(card1, card2):
-    return card1.compareHands(card2)
+        return 0
 
 # Part One
 
 hands = [Hand(line) for line in myInput]
-hands = sorted(hands, key=functools.cmp_to_key(compareHands))
+hands = sorted(hands, key=functools.cmp_to_key(lambda a,b: a.compareHands(b)))
 
 print(sum([hands[i].bid*(i+1) for i in range(len(hands))]))
 
 # Part Two
 
-hands = [Hand(line, True) for line in myInput]
-hands = sorted(hands, key=functools.cmp_to_key(compareHands))
+hands = [Hand(line, isPartTwo=True) for line in myInput]
+hands = sorted(hands, key=functools.cmp_to_key(lambda a,b: a.compareHands(b)))
 
 print(sum([hands[i].bid*(i+1) for i in range(len(hands))]))
