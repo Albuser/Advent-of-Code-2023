61c2b5c49f1c01aa54e2a4635ee72304df2466c9
Albuser <alexanderbuser@gmail.com>
2023-12-09 00:40:14 -0500

    cleanup

--- solutions/day_9.py
+++ solutions/day_9.py
@@ -1,12 +1,12 @@
 import re
 myInput = [line.strip() for line in open("inputs/day_9.txt").readlines()]
 
-def parseLine(line, endIndx=-1, factor=1):
+def parseLine(line, endIndx=-1, factor=1, offset=1):
     nums = [int(num) for num in re.findall(r'-?\d+', line)]
     extrapolated = nums[endIndx]
     while any([num != 0 for num in nums]):
         nums = [nums[i+1]-nums[i] for i in range(len(nums)-1)]
-        extrapolated += nums[endIndx]*(factor)**(len(nums)+1)
+        extrapolated += nums[endIndx]*(factor)**(len(nums)+offset)
     return extrapolated
 
 # Part One
@@ -14,5 +14,5 @@ def parseLine(line, endIndx=-1, factor=1):
 print(sum([parseLine(line) for line in myInput]))
 
 # Part Two
-
-print(sum([parseLine(line, 0, -1) for line in myInput]))
+# offset may be different for you
+print(sum([parseLine(line, endIndx=0, factor=-1, offset=1) for line in myInput]))
